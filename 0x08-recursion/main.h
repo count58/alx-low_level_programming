5ef88298b2277e462a4a57434197dd39875841f9
chuwa peter <chuwapeter@yahoo.com>
2023-04-20 07:03:45 -0700

    C - Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,9 +5,9 @@ void _puts_recursion(char *a);
 int _putchar(char c);
 void _print_rev_recursion(char *a);
 int _strlen_recursion(char *a);
-int factorial(int n);
+int factorial(int i);
 int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
+int _sqrt_recursion(int i);
+int is_prime_number(int i);
 int is_palindrome(char *a);
 #endif
